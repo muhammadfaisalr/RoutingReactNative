9f5f2c6bd0de6018d48e92c0655c1d734946d6fc
muhammadfaisalr <faisalbismania7@gmail.com>
2021-03-07 00:15:46 +0700

    Quiz13

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Tugas15 from './Tugas15/index'
+import Tugas15 from './Quiz13/index'
 
 export default function App() {
   return (
